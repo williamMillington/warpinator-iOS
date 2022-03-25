5293a4284d39b502ec6684785f956dcf5a81415c
williamMillington <willington1974@gmail.com>
2022-03-25 17:38:17 -0300

    Fixes accidental file renaming

--- Warpinator/SceneDelegate.swift
+++ Warpinator/SceneDelegate.swift
@@ -18,19 +18,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        print(DEBUG_TAG+"scene(willConnectTo: ...")
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
+
         let vc = UINavigationController()
-        
+
         coordinator = MainCoordinator(withNavigationController: vc) //LevelCoordinator(withNavigationController: vc)
-        
+
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = vc
         window?.makeKeyAndVisible()
-        
+
         coordinator?.start()
         
     }
